359d0d0c558d8e8634fbbc4f6b2dbf2fa8a9eb9f
rover <karenzyc@gmail.com>
2025-03-31 17:48:28 -0400

    Added code for moving arm based on transform from key

--- Tappy_ws/src/arm_control/setup.py
+++ Tappy_ws/src/arm_control/setup.py
@@ -1,25 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'arm_control'
+package_name = "arm_control"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='rover',
-    maintainer_email='karenzyc@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="rover",
+    maintainer_email="karenzyc@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "dynamic_arm = arm_control.dynamic_move_arm:main",
         ],
     },
 )
