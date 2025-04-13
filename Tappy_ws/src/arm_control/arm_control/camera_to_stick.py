fbaaa0bf6ef43e7bfd7b1a21aed981681876c74d
Karen Zhang <karenzyc@gmail.com>
2025-04-13 05:07:00 -0400

    Added mapping for camera, stick, all keys

--- Tappy_ws/src/arm_control/arm_control/camera_to_stick.py
+++ Tappy_ws/src/arm_control/arm_control/camera_to_stick.py
@@ -9,6 +9,8 @@ from scipy.spatial.transform import Rotation as R
 from tf2_ros.transform_broadcaster import TransformBroadcaster
 from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
 from turtlesim.msg import Pose
+from scipy.spatial.transform import Rotation
+import numpy as np
 
 
 class Camera_Stick_broadcaster(Node):
@@ -25,17 +27,18 @@ class Camera_Stick_broadcaster(Node):
 
     def on_timer(self):
         self.get_logger().info("Broadcasting pose of : camera and stick")
+        quaternion = Rotation.from_matrix(np.eye(3, 3)).as_quat()
         t = TransformStamped()
         t.header.stamp = self.get_clock().now().to_msg()
-        t.header.frame_id = "rx200/ee_gripper_link"
+        t.header.frame_id = "rx200/fingers_link"
         t.child_frame_id = "stick_end"
         t.transform.translation.x = 0.09398
         t.transform.translation.y = 0.0
         t.transform.translation.z = 0.0
-        t.transform.rotation.x = 1.0
-        t.transform.rotation.y = 1.5
-        t.transform.rotation.z = 0.0
-        t.transform.rotation.w = 0.0
+        t.transform.rotation.x = quaternion[0]
+        t.transform.rotation.y = quaternion[1]
+        t.transform.rotation.z = quaternion[2]
+        t.transform.rotation.w = quaternion[3]
 
         self.tf_static_broadcaster.sendTransform(t)
 
@@ -46,10 +49,10 @@ class Camera_Stick_broadcaster(Node):
         tfs.transform.translation.x = -0.08128
         tfs.transform.translation.y = 0.01778
         tfs.transform.translation.z = 0.0381
-        tfs.transform.rotation.x = 1.0
-        tfs.transform.rotation.y = 1.5
-        tfs.transform.rotation.z = 0.0
-        tfs.transform.rotation.w = 0.0
+        tfs.transform.rotation.x = quaternion[0]
+        tfs.transform.rotation.y = quaternion[1]
+        tfs.transform.rotation.z = quaternion[2]
+        tfs.transform.rotation.w = quaternion[3]
 
         self.tf_static_broadcaster.sendTransform(tfs)
 
